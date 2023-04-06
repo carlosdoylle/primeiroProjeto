c5172d3152747609899499a98bc248f45a4b05b9
carlos <junior.pessoaagradavel@gmail.com>
2023-04-05 21:54:54 -0300

    primeiro projeto HTML e JS

---
+++ script.js
@@ -0,0 +1,23 @@
+//qualquer texto
+ console.log("ola mundo");
+ prompt("qual sua idade?")
+  //comentario estremamente aleatorio de linha
+  /* comentario 
+    estremamente aleatorio
+    de bloco */
+
+         const       novavariavel  =            100
+    //    ^               ^        ^             ^
+    // declaraçao      nome      atribuiçao    valor
+
+const nome = 'carlos';
+const nome2 = "ferreira";
+let idade = 16;
+const nascimento = "06/15/23"
+const texto = "meu nome é"
+const souEstudante = true
+
+
+console.log("data de nascimento:" , nascimento);
+console.log(texto , nome);
+console.log("meu nome é" , nome , nome2 , "tenho" , idade, "anos, sou estudante?" , souEstudante);
